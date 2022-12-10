45f32a761e0cca484f5b386f027a3ee00b59814e
Biswaranjan Padhy <padhybiswaranjan@gmail.com>
2022-12-10 22:14:46 +0530

    feat: added health check

---
+++ controller/health.js
@@ -0,0 +1,35 @@
+import { checkDatabaseHealth, checkSendgridHealth, secondsToDuration } from "../lib/utils";
+
+export const serviceHealthStatus = async (req) => {
+  // Response time measure start
+  const start = process.hrtime();
+  const { httpVersion } = req;
+
+  // Check database and send grid mail API health
+  const [isDatabaseHealthy, isSendGridEmailAPIHealthy] = await Promise.all([
+    checkDatabaseHealth(),
+    checkSendgridHealth(),
+  ]);
+
+  let status;
+  if (isDatabaseHealthy && isSendGridEmailAPIHealthy)
+    status = { state: "OPERATIONAL", description: "All systems operational" };
+  else if (isDatabaseHealthy || isSendGridEmailAPIHealthy) {
+    status = { state: "DEGRADED" };
+    status.description = isDatabaseHealthy ? "Mail API is currently having issues" : "Database service unavailable";
+  } else status = { state: "OUTAGE", description: "Major systems outage" };
+
+  // Response time measure end
+  const end = process.hrtime(start);
+  status.updated_at = new Date().toISOString();
+
+  return {
+    server: {
+      httpVersion,
+      pid: process.pid,
+      uptime: secondsToDuration(process.uptime()),
+      response_time: `${(end[0] * 1000 + end[1] / 1000000).toFixed(2)}ms`,
+    },
+    status,
+  };
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const authRoute = require("./auth");
+const { servicesHealthCheck } = require("./healthCheck");
 
 // Auth routes
 router.post("/signup", authRoute.signUp);
 router.get("/signin", authRoute.signIn);
 router.post("/generate_otp", authRoute.generateOTP);
 router.post("/update_password", authRoute.updatePassword);
+
+// Health check
+router.get("/health", servicesHealthCheck);
 
 module.exports = router;
